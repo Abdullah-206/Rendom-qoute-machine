3cfb2b4c059a92a39fa0a225c133afba87212160
Abdullah-206 <abdullahashfaq206@gmail.com>
2023-01-03 10:07:32 -0800

    rendom-qoute

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react'
+const quoteData = [
+  { text: `“The purpose of our lives is to be happy.”`, author: "Dalai Lama" },
+  { text: `“Life is what happens when youre busy making other plans.”`, author: "John Lennon" },
+  { text: `“Get busy living or get busy dying.”`, author: "Stephen King" },
+  { text: `“You only live once, but if you do it right, once is enough.”`, author: "Mae West" },
+  { text: ` “Many of lifes failures are people who did not realize how close they were to success when they gave up.”`, author: "Thomas A. Edison" },
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const QuoteBox = ({ quote, handleNewQuote }) => (
+  <div id="quote-box">
+    <p id="text">{quote.text}</p>  
+    <h2 id="author">{quote.author}</h2>
+    <div class="actions">
+      <button 
+        id="new-quote" 
+        class="button"
+        onClick={handleNewQuote} 
         >
-          Learn React
-        </a>
-      </header>
+        New Quote
+      </button>
+      <a 
+         href="https://twitter.com/intent/tweet"
+         id="tweet-quote"
+         rel="noreferrer"
+       >
+         Tweet
+      </a>
     </div>
-  );
+  </div>
+);
+
+const getRandomIndex = (max) => 
+  Math.round(Math.random() * ((quoteData.length-1) - 0) + 0);
+
+const App = () => {
+  const [quote, setQuote] = React.useState(quoteData[getRandomIndex()])
+
+  const handleNewQuote = () => {
+    setQuote(quoteData[getRandomIndex()])
+  }
+
+  return (
+    <div class="main">
+      <QuoteBox handleNewQuote={handleNewQuote} quote={quote} />
+    </div>
+  )
 }
+
+ReactDOM.render(<App />, document.querySelector("#app"));
+
 
 export default App;
